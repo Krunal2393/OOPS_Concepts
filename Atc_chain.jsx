a6fac242f6e3618e0a01181fb9e0caafef518586
KrunalBhanushali-tecblic <krunalbhanushali.tecblic@gmail.com>
2023-02-09 10:58:19 +0530

    krunal Rocks

---
+++ Atc_chain.jsx
@@ -0,0 +1,177 @@
+const parts = [
+  {
+    top: "67%",
+    left: "25%",
+    title: "Conveyor Support Base Components",
+    opacity: 1,
+    bgColor: "skyBlue",
+    activeBgColor: "blue",
+    imageUrl: [
+      {
+        image:'/assets/images/conveyor1',
+       
+      },
+      {
+        image:'/assets/images/conveyor2',
+       
+      },
+      {
+        image:'/assets/images/conveyor3',
+       
+      },
+      {
+        image:'/assets/images/conveyor4',
+       
+      },
+      {
+        image:'/assets/images/conveyor5',
+       
+      },
+      {
+        image:'/assets/images/conveyor6',
+       
+      },
+
+    ],
+  },
+  {
+    top: "54%",
+    left: "39%",
+    opacity: 1,
+    bgColor: "skyBlue",
+    activeBgColor: "blue",
+    title: "Flexible Chain & Assembly",
+
+    imageUrl: [
+      {
+        image:'/assets/images/assembly1',
+       
+      },
+      {
+        image:'/assets/images/assembly2',
+       
+      },
+      {
+        image:'/assets/images/assembly3',
+       
+      },
+      {
+        image:'/assets/images/assembly4',
+       
+      },
+      {
+        image:'/assets/images/assembly5',
+       
+      },
+      {
+        image:'/assets/images/assembly6',
+       
+      },
+      {
+        image:'/assets/images/assembly7',
+       
+      },
+      {
+        image:'/assets/images/assembly8',
+       
+      },
+
+    ],
+  },
+  {
+    top: "76%",
+    left: "68%",
+    opacity: 1,
+    bgColor: "skyBlue",
+    activeBgColor: "blue",
+    title: "SS & Plastic Chains",
+  },
+  {
+    top: "83%",
+    left: "65%",
+    opacity: 1,
+    bgColor: "skyBlue",
+    activeBgColor: "blue",
+    title: "Conveyor Bearings",
+    imageUrl: [
+      {
+        image:'/assets/images/bearing1',
+       
+      },
+      {
+        image:'/assets/images/bearing2',
+       
+      },
+      {
+        image:'/assets/images/bearing3',
+       
+      },
+      {
+        image:'/assets/images/bearing4',
+       
+      },
+      {
+        image:'/assets/images/bearing5',
+       
+      },
+     
+
+    ],
+  },
+  {
+    top: "67%",
+    left: "68%",
+    opacity: 1,
+    bgColor: "skyBlue",
+    activeBgColor: "blue",
+    title: "Side Brackets & Connecting Joints Strips",
+    imageUrl: [
+      {
+        image:'/assets/images/joint1',
+       
+      },
+      {
+        image:'/assets/images/joint2',
+       
+      },
+      {
+        image:'/assets/images/joint3',
+       
+      },
+      {
+        image:'/assets/images/joint4',
+       
+      },
+      {
+        image:'/assets/images/joint5',
+       
+      },
+     
+
+    ],
+  },
+  {
+    top: "54%",
+    left: "63%",
+    opacity: 1,
+    bgColor: "skyBlue",
+    activeBgColor: "blue",
+    title: "Straigh & Inclined Modular Belt & Sprocket",
+  },
+  {
+    top: "43%",
+    left: "69%",
+    opacity: 1,
+    bgColor: "skyBlue",
+    activeBgColor: "blue",
+    title: "Conveyor Rollers",
+  },
+  {
+    top: "67%",
+    left: "29%",
+    opacity: 1,
+    bgColor: "skyBlue",
+    activeBgColor: "blue",
+    title: "Bucket Elevator & Assembly",
+  },
+];
